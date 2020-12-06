6380d441469377cc82e090c409fac9979f13ec1f
from97 <skm1104@daum.net>
2020-12-06 21:20:16 +0900

    2.0-2.8

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
+//const express = require('express'); === import express
+const app = express(); //call express & make express application
+
+const handleHome = (req, res) => res.send('HI from home');
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
+
+export default app;
