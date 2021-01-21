e12e6b3eafb2b39ba52ff67f0b6ecb4119b5eb8a
from97 <skm1104@daum.net>
2021-01-21 15:15:05 +0900

    modify userRouter.js, app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import { userRouter } from "./routers/userRouter";
 //const express = require('express'); === import express
 const app = express(); //call express & make express application
 
