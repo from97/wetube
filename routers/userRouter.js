72a9dd571b16867b122fc91345b4e0fc381d1dee
from97 <skm1104@daum.net>
2021-01-21 16:16:36 +0900

    get user routers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,4 +2,11 @@ import express from "express";
 
 const userRouter = express.Router();
 
+userRouter.get(routes.users, (res, req) => res.send("Users"));
+userRouter.get(routes.userDetail, (res, req) => res.send("User Detail"));
+userRouter.get(routes.editProfile, (res, req) => res.send("Edit Profile"));
+userRouter.get(routes.changePassword, (res, req) =>
+  res.send("Change Password")
+);
+
 export default userRouter;
