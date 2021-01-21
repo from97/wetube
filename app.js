5ae2a6c9311a0df697a438579e6f6623eeb200e2
from97 <skm1104@daum.net>
2021-01-21 15:59:47 +0900

    import routes in app.js & get urls in globalRouters.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
 //const express = require('express'); === import express
 const app = express(); //call express & make express application
 
@@ -15,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/users", userRouter);
-app.use("/videos", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
